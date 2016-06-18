463946269441c158ae3d07f43ddc6af889c8c52d
Igor Shvedenenko <igor_sh@inisoft.com.ua>
2016-06-19 00:50:33 +0300

    Layout implemented

--- app/src/main/java/com/weatherinfo/Weather.java
+++ app/src/main/java/com/weatherinfo/Weather.java
@@ -5,12 +5,9 @@ import android.content.IntentSender;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
@@ -35,7 +32,6 @@ public class Weather extends AppCompatActivity implements
         LocationListener,
         ResultCallback<LocationSettingsResult>, Constants {
 
-    // {"_id":700569,"name":"Mykolayiv","country":"UA","coord":{"lon":31.9974,"lat":46.965912}}
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
     private LocationSettingsRequest mLocationSettingsRequest;
@@ -48,14 +44,7 @@ public class Weather extends AppCompatActivity implements
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         locationInit();
     }
 
