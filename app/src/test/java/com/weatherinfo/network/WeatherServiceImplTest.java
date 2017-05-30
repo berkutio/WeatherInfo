52120bceda2f83c8e04eeeb62a12c0f6249c0b8b
user <shvedenenkoio@gmail.com>
2017-05-30 13:34:43 +0300

    Rewritten network test

--- app/src/test/java/com/weatherinfo/network/WeatherServiceImplTest.java
+++ app/src/test/java/com/weatherinfo/network/WeatherServiceImplTest.java
@@ -39,40 +39,24 @@ public class WeatherServiceImplTest {
 
     @Test
     public void testGetListData() throws IOException {
-        service.getListData(location, 6)
-                .subscribeWith(new DisposableObserver<WeatherResponse>() {
-                    @Override
-                    public void onNext(WeatherResponse response) {
-                        assertNotNull(response);
-                        System.out.println("Weather response + " + response);
-                        City city = response.getCity();
-                        assertNotNull(city);
-                        assertEquals("Mykolayiv", city.getName());
-                        ForecastData[] list = response.getList();
-                        assertNotNull(list);
-                        assertEquals(6, list.length);
-                        ForecastData forecastData = list[0];
-                        assertNotNull(forecastData);
-                        WeatherDescription[] descriptions = forecastData.getWeather();
-                        assertNotNull(descriptions);
-                        assertNotEquals(0, descriptions.length);
-                        assertNotNull(descriptions[0].getDescription());
-                        assertNotEquals(0, forecastData.getDt());
-                        assertNotEquals(0, forecastData.getDeg());
-                        assertNotEquals(0, forecastData.getHumidity());
-                        assertNotEquals(0, forecastData.getPressure());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        fail(e.getMessage());
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
+        WeatherResponse response = service.getListData(location, 6).blockingSingle();
+        assertNotNull(response);
+        System.out.println("Weather response + " + response);
+        City city = response.getCity();
+        assertNotNull(city);
+        ForecastData[] list = response.getList();
+        assertNotNull(list);
+        assertEquals(6, list.length);
+        ForecastData forecastData = list[0];
+        assertNotNull(forecastData);
+        WeatherDescription[] descriptions = forecastData.getWeather();
+        assertNotNull(descriptions);
+        assertNotEquals(0, descriptions.length);
+        assertNotNull(descriptions[0].getDescription());
+        assertNotEquals(0, forecastData.getDt());
+        assertNotEquals(0, forecastData.getDeg());
+        assertNotEquals(0, forecastData.getHumidity());
+        assertNotEquals(0, forecastData.getPressure());
     }
 
 }
