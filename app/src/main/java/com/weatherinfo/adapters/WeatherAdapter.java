8b9957069e3c1ce8b0d741e0b22e3fb783196e4d
user <shvedenenkoio@gmail.com>
2017-06-12 15:21:46 +0300

    Implemented one-way databinding in WeatherAdapter

--- app/src/main/java/com/weatherinfo/adapters/WeatherAdapter.java
+++ app/src/main/java/com/weatherinfo/adapters/WeatherAdapter.java
@@ -1,17 +1,15 @@
 package com.weatherinfo.adapters;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import com.weatherinfo.R;
-import com.weatherinfo.model.ForecastData;
-import com.weatherinfo.utils.Universal;
-import java.util.List;
 
-import butterknife.BindView;
+import com.weatherinfo.databinding.WeatherMappingItemBinding;
+import com.weatherinfo.model.DataBindingForecastData;
+import com.weatherinfo.model.ForecastData;
+import java.util.List;
 
 public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
 
@@ -19,23 +17,11 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView data;
-        public TextView general;
-        public TextView temperature;
-        public TextView pressure;
-        public TextView humidity;
-        public TextView windSpeed;
-        public ImageView imageView;
+        private WeatherMappingItemBinding binding;
 
         public ViewHolder(View v) {
             super(v);
-            imageView = (ImageView)v.findViewById(R.id.im_description);
-            data = (TextView)v.findViewById(R.id.data);
-            general = (TextView)v.findViewById(R.id.general_description);
-            temperature = (TextView)v.findViewById(R.id.temperature_val);
-            pressure = (TextView)v.findViewById(R.id.pressure_val);
-            humidity = (TextView)v.findViewById(R.id.humidity_val);
-            windSpeed = (TextView)v.findViewById(R.id.wind_speed_val);
+            binding = DataBindingUtil.bind(v);
         }
     }
 
@@ -46,23 +32,16 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
     // Create new views (invoked by the layout manager)
     @Override
     public WeatherAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.weather_mapping_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        WeatherMappingItemBinding binding = WeatherMappingItemBinding.inflate(layoutInflater, parent, false);
+        return new ViewHolder(binding.getRoot());
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         ForecastData forecastData = list.get(position);
-        holder.imageView.setImageResource(Universal.getWeatherResource(forecastData.getWeather()[0].getDescription()));
-        holder.data.setText(Universal.formatData(forecastData.getDt()));
-        holder.general.setText(forecastData.getWeather()[0].getDescription());
-        holder.temperature.setText(Universal.convertToCelicies(forecastData.getTemp().getMin()));
-        holder.pressure.setText(String.valueOf(forecastData.getPressure()));
-        holder.humidity.setText(String.valueOf(forecastData.getHumidity()));
-        holder.windSpeed.setText(String.valueOf(forecastData.getSpeed()));
+        holder.binding.setForecast(new DataBindingForecastData(forecastData));
     }
 
 
